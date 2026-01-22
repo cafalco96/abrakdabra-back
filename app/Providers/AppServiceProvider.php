735f1e20910aa59dfc8cf24525ec484b94de97c6
cafalco96 <carlossfalco96@gmail.com>
2026-01-22 02:03:16 -0500

    add services

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Auth\Notifications\ResetPassword;
+use App\Models\User;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        ResetPassword::createUrlUsing(function (User $user, string $token) {
+            $frontendUrl = config('app.frontend_url');
+            return $frontendUrl.'/auth/reset-password?token='.$token.'&email='.urlencode($user->getEmailForPasswordReset());
+        });
+    }
+}
